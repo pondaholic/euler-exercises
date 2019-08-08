8cce358018608c8e6b84a62877043f301b924efb
Shelly <shelly@joinreel.com>
2019-08-08 14:19:38 -0700

    check to see if number is prime

--- three.js
+++ three.js
@@ -4,9 +4,27 @@ What is the largest prime factor of the number 600851475143 ? */
 // primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
 
 function prime(number) {
-	for (let i = 2; i < number; i++) {
-		Math.ceil(Math.sqrt(i));
+	//is this number prime?
+	isPrime = true;
+	sqroot = Math.ceil(Math.sqrt(number));
+	for (let i = 2; i < sqroot; i++) {
+		if (number % i === 0) {
+			return (isPrime = false);
+		}
+	}
+	isPrime ? number : '';
+}
+
+function findLargestPrime(number) {
+	if (!prime(number)) {
+		return number / 2;
 	}
 }
 
-prime(13195);
+// prime(2);
+// prime(3);
+// prime(14);
+// prime(13);
+// prime(27);
+
+console.log(findLargestPrime(14));
