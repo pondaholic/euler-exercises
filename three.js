5fba64c5cc0c4d544723b1bef03a5db52b510e83
Shelly <shelly@joinreel.com>
2019-08-07 17:58:08 -0700

    stuck on primes

--- three.js
+++ three.js
@@ -1,3 +1,8 @@
 /* The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ? */
+// primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+
+function prime(number) {}
+
+// prime(13195);
