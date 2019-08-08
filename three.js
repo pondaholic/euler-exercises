ba9ba7a7611aa8b8daedf865cfc88ed221cf29a7
Shelly <shelly@joinreel.com>
2019-08-07 18:05:06 -0700

    trying prime

--- three.js
+++ three.js
@@ -3,6 +3,10 @@
 What is the largest prime factor of the number 600851475143 ? */
 // primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
 
-function prime(number) {}
+function prime(number) {
+	for (let i = 2; i < number; i++) {
+		Math.ceil(Math.sqrt(i));
+	}
+}
 
-// prime(13195);
+prime(13195);
